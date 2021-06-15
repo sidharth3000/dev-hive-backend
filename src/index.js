7b813c08b17ee0092a4927943cc37eccfe6d0dd6
sidharth <201952234@iiitvadodara.ac.in>
2021-06-16 04:35:16 +0530

    chat init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 var bodyParser = require('body-parser')
-const cors = require('cors');
+const cors = require('cors')
+const http = require('http')
+const socketio = require('socket.io')
 
 const userRouter = require('./routes/user')
 const postsRouter = require('./routes/posts')
@@ -9,14 +11,43 @@ const commentRouter = require('./routes/comment')
 require('./db/mongoose')
          
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
+
 const port = process.env.PORT || 9000
 
+app.use(cors({ origin: '*'}));
 app.use(express.json())
 app.use(cors({ origin: '*'}));
 app.use(userRouter)
 app.use(postsRouter)
 app.use(commentRouter)
 
-app.listen(port, () =>{
+io.on('connection', (socket) => { 
+   
+    socket.emit('connection', "Welcome");
+    socket.broadcast.emit('messsage', 'A new use has joined')
+
+    socket.on('sendMessage', (mssg) => {
+        io.emit('message', mssg)
+    });   
+
+    socket.on('sendLocation', (location) => {
+        io.emit('message', `https://google.com/maps?=${location.latitude},${location.longitude}`)
+    });   
+
+    socket.on('disconnect', () => {
+        io.emit('messsage', 'a user has left')
+    })
+});
+
+
+server.listen(port, () =>{
     console.log("connected!");
 })
